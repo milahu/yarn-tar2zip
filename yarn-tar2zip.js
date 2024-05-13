db40166e50290353b33fb01e016ec7f1fb3820e5
Milan Hauth <milahu@gmail.com>
2024-05-13 15:36:54 +0200

    clean

--- yarn-tar2zip.js
+++ yarn-tar2zip.js
@@ -1,44 +1,44 @@
-import { PortablePath, NodeFS, ppath, xfs, npath, constants, statUtils} from '@yarnpkg/fslib';
-import { ZipFS}                                                 from '@yarnpkg/libzip';
-import {PassThrough,}                                                 from 'stream';
-import * as tar                                                                     from 'tar';
+import { PortablePath, ppath, npath, constants, statUtils} from '@yarnpkg/fslib';
+import { ZipFS } from '@yarnpkg/libzip';
+import { PassThrough } from 'stream';
+import * as tar from 'tar';
 import fs from 'fs';
 
 // https://betterprogramming.pub/a-memory-friendly-way-of-reading-files-in-node-js-a45ad0cc7bb6
 function readBytes(fd, sharedBuffer) {
-    return new Promise((resolve, reject) => {
-        fs.read(
-            fd,
-            sharedBuffer,
-            0,
-            sharedBuffer.length,
-            null,
-            (err) => {
-                if(err) { return reject(err); }
-                resolve();
-            }
-        );
-    });
+  return new Promise((resolve, reject) => {
+    fs.read(
+      fd,
+      sharedBuffer,
+      0,
+      sharedBuffer.length,
+      null,
+      (err) => {
+        if(err) { return reject(err); }
+        resolve();
+      }
+    );
+  });
 }
 
 // https://betterprogramming.pub/a-memory-friendly-way-of-reading-files-in-node-js-a45ad0cc7bb6
 async function* generateChunks(filePath, size) {
-    const sharedBuffer = Buffer.alloc(size);
-    const stats = fs.statSync(filePath); // file details
-    const fd = fs.openSync(filePath); // file descriptor
-    let bytesRead = 0; // how many bytes were read
-    let end = size;
+  const sharedBuffer = Buffer.alloc(size);
+  const stats = fs.statSync(filePath); // file details
+  const fd = fs.openSync(filePath); // file descriptor
+  let bytesRead = 0; // how many bytes were read
+  let end = size;
 
-    for(let i = 0; i < Math.ceil(stats.size / size); i++) {
-        await readBytes(fd, sharedBuffer);
-        bytesRead = (i + 1) * size;
-        if(bytesRead > stats.size) {
-           // When we reach the end of file,
-           // we have to calculate how many bytes were actually read
-           end = size - (bytesRead - stats.size);
-        }
-        yield sharedBuffer.slice(0, end);
+  for(let i = 0; i < Math.ceil(stats.size / size); i++) {
+    await readBytes(fd, sharedBuffer);
+    bytesRead = (i + 1) * size;
+    if(bytesRead > stats.size) {
+     // When we reach the end of file,
+     // we have to calculate how many bytes were actually read
+     end = size - (bytesRead - stats.size);
     }
+    yield sharedBuffer.slice(0, end);
+  }
 }
 
 //async function * parseTar(tgz) {
